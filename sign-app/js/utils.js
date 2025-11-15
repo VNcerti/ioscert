a34bb1fe52bb5b8e09b5de11e6dd0cc6264430ff
VNcerti <tlvdzreal@gmail.com>
2025-11-15 20:59:32 +0700

    Update utils.js

--- sign-app/js/utils.js
+++ sign-app/js/utils.js
@@ -1,19 +1,2 @@
-// Utility Functions
-
-// Hàm tạo ID ngắn (6 ký tự)
-function generateShortId() {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
-    for (let i = 0; i < 6; i++) {
-        result += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
-    return result;
-}
-
-// API URLs - ĐẢM BẢO ĐÂY LÀ GLOBAL VARIABLES
-var SignUrl = 'https://sign.ipasign.cc/api/sign';
-var StatusUrl = 'https://sign.ipasign.cc/api/status'; 
-var DownloadUrl = 'https://sign.ipasign.cc/api/download';
-
-// Debug: Log để kiểm tra
-console.log('API URLs loaded:', { SignUrl, StatusUrl, DownloadUrl });
+// File này không cần thiết nữa vì đã chuyển toàn bộ vào app.js
+// Có thể xóa file này hoặc để trống
