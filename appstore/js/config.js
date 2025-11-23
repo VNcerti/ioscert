f0af1d1ebfa9f9bb0a6a9d4171c3133e0bdda5c0
VNcerti <tlvdzreal@gmail.com>
2025-11-23 10:29:24 +0700

    Create config.js

---
+++ appstore/js/config.js
@@ -0,0 +1,14 @@
+// Configuration variables
+const CONFIG = {
+    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzTpJOSVLfa_8EzUMctCFOicNU6QwG5uPBtIUDrgG7urIdBZ5PhkwZEw0Lq7QTYKOCRXw/exec',
+    CACHE_KEY: 'xspace_apps_cache',
+    CACHE_TIMESTAMP_KEY: 'xspace_cache_timestamp',
+    CACHE_DURATION: 30 * 60 * 1000, // 30 phút
+    CATEGORY_LABELS: {
+        'game': 'Trò chơi',
+        'social': 'Mạng xã hội',
+        'entertainment': 'Giải trí',
+        'photo': 'Chụp ảnh',
+        'clone': 'Nhân bản'
+    }
+};
