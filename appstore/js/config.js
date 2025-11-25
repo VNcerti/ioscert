3687c209f54dfce71b3589d2b15eac384ad91dcd
VNcerti <tlvdzreal@gmail.com>
2025-11-25 21:16:02 +0700

    Update config.js

--- appstore/js/config.js
+++ appstore/js/config.js
@@ -1,6 +1,6 @@
 // Configuration variables
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzTpJOSVLfa_8EzUMctCFOicNU6QwG5uPBtIUDrgG7urIdBZ5PhkwZEw0Lq7QTYKOCRXw/exec',
+    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxyu0jB4zWWRWPx1RW_D2J6A5NLrTGyq9-WaQ3Pc1BWhRSm4e7IYnMEpFohKXv6C7dJTA/exec',
     CACHE_KEY: 'xspace_apps_cache',
     CACHE_TIMESTAMP_KEY: 'xspace_cache_timestamp',
     CACHE_DURATION: 30 * 60 * 1000, // 30 phút
