30132955d12b2a742447415ad37187f5d7d9e223
VNcerti <tlvdzreal@gmail.com>
2025-11-29 01:16:08 +0700

    Update config.js

--- appstore/js/config.js
+++ appstore/js/config.js
@@ -1,6 +1,6 @@
 // Configuration variables
 const CONFIG = {
-    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwj2PcAuZnzDPZKvc8yZFCIJTj7u2foym6dO9HuCVcPkRaOqW5oU6PbDQyuTUsXQCed4w/exec',
+    GOOGLE_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbywHM36jEQpXFXzBTdG-EcbdbKWbtMyKeOSjNwUt-XahtstAmm1F_R2fr8JWUms_A1jsQ/exec',
     CACHE_KEY: 'xspace_apps_cache',
     CACHE_TIMESTAMP_KEY: 'xspace_cache_timestamp',
     CACHE_DURATION: 30 * 60 * 1000, // 30 phút
@@ -9,6 +9,7 @@ const CONFIG = {
         'social': 'Mạng xã hội',
         'entertainment': 'Giải trí',
         'photo': 'Chụp ảnh',
-        'clone': 'Nhân bản'
+        'clone': 'Nhân bản',
+        'premium': 'Mở khoá Premium'
     }
 };
